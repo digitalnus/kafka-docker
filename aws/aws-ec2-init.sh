619c042dc12131c493adabc78045d5a3a029b32d
Alfred Leong <87219263+digitalnus@users.noreply.github.com>
2021-08-13 07:03:01 -0400

    Create aws-ec2-init.sh
    Init script for AWS EC2 instance creation that will install Docker, Docker Compose and run Kafka and Zookeeper

---
+++ aws/aws-ec2-init.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Install docker
+sudo yum update -y
+sudo yum install docker -y
+sudo service docker start
+sudo docker -v
+
+#
+# Add ec2-user to the docker group
+#
+sudo usermod -aG docker $USER
+
+# Install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+# Set execute permission to docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+#
+# Enable docker to start up automatically after reboot
+#
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
+
+# Install git 
+sudo yum install git -y
+
+# Clone Kafka project into directory
+git clone https://github.com/digitalnus/kafka-docker.git -b master
+
+#
+# Start up Kafka and Zookeeper
+#
+cd kafka-docker
+sudo docker-compose up -d 
