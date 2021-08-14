1295937e59824cc972a8748eec11dcad219708dd
digitalnus <cool@outlook.sg>
2021-08-14 14:59:04 -0400

    Added MyProducer class to com.github.digitalnus.demo.kafka.tutorial1 package

---
+++ java/kafka-demo/src/main/java/com/github/digitalnus/demo/kafka/tutorial1/MyProducer.java
@@ -0,0 +1,8 @@
+package com.github.digitalnus.demo.kafka.tutorial1;
+
+public class MyProducer {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
